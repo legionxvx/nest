bd8da816e8fed2e5756a6b595ededf030329dc20
Nikolaus Gullotta <nik@harrisonconsoles.com>
2020-02-26 09:05:02 -0600

    Trim down and shuffle Pipfile and update requirements
    * Move alembic to dev packages
    * Remove redis, Relock provides this
    * Trim down and rename setup requirements

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
 from setuptools import setup
 
-install_requires = [
-    "alembic>=1.4.0",
+requirements = [
     "certifi>=2019.11.28",
     "chardet>=3.0.4",
     "colorlog>=4.1.0",
     "idna>=2.9",
-    "mako>=1.1.1",
-    "markupsafe>=1.1.1",
     "psycopg2-binary>=2.8.4",
-    "python-dateutil>=2.8.1",
-    "python-editor>=1.0.4",
     "redis>=3.4.1",
     "redlock>=1.2.0",
     "requests>=2.23.0",
-    "six>=1.14.0",
     "sqlalchemy>=1.3.13",
     "urllib3>=1.25.8",
 ]
 
 setup(
-    name="Foo",
+    name="Nest",
     version="0.1",
-    description="Lorem ipsum",
+    description="",
     author="Nikolaus Gullotta",
-    packages=['nest'],
-    install_requires=install_requires,
+    packages=["nest"],
+    install_requires=requirements,
 )
