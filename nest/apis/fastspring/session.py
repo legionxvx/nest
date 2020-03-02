da52ae21fea87b6c31794fee1f4ced855e2fdb31
Nikolaus Gullotta <nik@harrisonconsoles.com>
2020-03-02 15:54:54 -0600

    Applying some finishing touches to FastSpring custom session

--- nest/apis/fastspring/session.py
+++ nest/apis/fastspring/session.py
@@ -1,5 +1,6 @@
 import logging
 from collections import defaultdict
+from functools import wraps
 from json import JSONDecodeError
 from os import environ
 from urllib.parse import urljoin
@@ -7,60 +8,145 @@ from urllib.parse import urljoin
 from requests import HTTPError, Session
 
 
+def protect(default=None):
+    """This decorator can be used on any method the requests some
+    resource and expects:
+
+    A) The response status to be non 4XX and non 5XX (aka, response OK)
+    B) The response content to be parsable JSON
+
+    In the event the method throws these errors uncaught, it will
+    catch these errors (HTTPError and JSONDecodeError) and return
+    the ``default`` value.
+
+        :param default=None: Default value to return if exceptions are
+        raised.
+    """
+    def wrapper(fn, *args, **kwargs):
+        @wraps(fn)
+        def wrapped(self, *args, **kwargs):
+            rv = None
+            try:
+                rv = fn(self, *args, **kwargs)
+            except (HTTPError) as error:
+                url = {error.response.url}
+                self.logger.error(f"Could not get {url}: {error}")
+            except (JSONDecodeError) as error:
+                self.logger.error(f"Could not decode response JSON: {error}")
+            return rv or default
+        return wrapped
+    return wrapper
+
 class FastSpring(Session):
-    """docstring here
+    """A custom ``Session`` to interact with FastSpring's API.
     """
     def __init__(self, prefix=None, auth=None, hooks={}):
         super().__init__()
         self.logger = logging.getLogger("nest")
         self.hooks = hooks
         self.auth = auth or (
-            environ.get("FS_AUTH_USER", ""), 
+            environ.get("FS_AUTH_USER", ""),
             environ.get("FS_AUTH_PASS", "")
         )
-        self.auth = (self.auth[0], self.auth[1])
 
     @property
     def prefix(self):
+        """URL to prefix on all requests. Since this is dedicated to
+        FastSpring, it is (currently): 'https://api.fastspring.com'.
+
+        This property is *read-only* and has no setter.
+        """
         return "https://api.fastspring.com"
 
-    def request(self, method, url, *args, **kwargs):
-        url = urljoin(self.prefix, "/".join([url]))
-        return super().request(method, url, *args, **kwargs)
+    def request(self, method, suffix, *args, **kwargs):
+        """Just like a noram ``Session.request()`` except that the
+        ``url`` is constructed using ``prefix`` and ``suffix``
 
-    def get_products(self, *args, **kwargs):
-        plist = ",".join(args)
-        res = self.request("GET", f"products/{plist}", **kwargs)
+            :param method: HTTP Verb
+            :param suffix: Appended to ``prefix``
+            :param *args: Passed to ``super`` request method
+            :param **kwargs: Passed to ``super`` request method
+        """
+        endpoint = urljoin(self.prefix, suffix)
+        return super().request(method, endpoint, *args, **kwargs)
 
-        try:
+    @protect(default={})
+    def get_products(self, filter=[], blacklist=[], *args, **kwargs):
+        """Request the "products" endpoint.
+
+        The ``products`` endpoint returns information about products
+        like their parents, price, offerings, description etc.
+
+            :param filter=[]: List of product id's
+            :param blacklist=[]: Offer type blacklist, useful for
+            removing things like bundles
+            :param *args: Passed to each ``get()`` request
+            :param **kwargs: Passed to each ``get()`` request
+        """
+        # Endpoint accepts comma-seperated list of product names
+        joined_ids = ",".join(filter)
+        res = self.get(f"products/{joined_ids}", *args, **kwargs)
+        res.raise_for_status()
+        data = res.json()
+
+        # `Filter` must've been empty, do the same request with the
+        # list of product ids in response
+        if len(data) > 1:
+            joined_ids = ",".join(data.get("products", []))
+            res = self.get(f"products/{joined_ids}", *args, **kwargs)
             res.raise_for_status()
             data = res.json()
-        except (HTTPError) as error:
-            self.logger.error(f"Could not get orders: {error}")
-            return []
-        except (JSONDecodeError) as error:
-            self.logger.error(f"Could not decode response JSON: {error}")
-            return []
-        
-        for product in data.get("products", []):
-            yield product
 
-    def get_events(self, type, **kwargs):
-        res = self.get(f"events/{type}", **kwargs)
+        # Stitch together the information
+        products = defaultdict(dict)
+        for info in data.get("products", []):
+            if info.get("result") != "success":
+                continue
 
-        try:
-            res.raise_for_status()
-            data = res.json()
-        except (HTTPError) as error:
-            self.logger.error(f"Could not get orders: {error}")
-            return []
-        except (JSONDecodeError) as error:
-            self.logger.error(f"Could not decode response JSON: {error}")
-            return []
+            if len(blacklist) > 0:
+                offers = info.get("offers", [])
+                # Skip offer-types in blacklist
+                if any([offer.get("type") in blacklist for offer in offers]):
+                    continue
 
-        yield data.get("events", [])
+            pricing = info.get("pricing", {})
+            price = pricing.get("price", {})
+            alias = info.get("product")
+            parent = info.get("parent")
 
-        # Drop any unnecessary params
+            # If there is no parent for this product, the alias must
+            # be the product itself
+            if not(parent):
+                parent = alias
+                products[parent]["price"] = price.get("USD", 0)
+
+            if parent and alias:
+                info = products[parent]
+                info["aliases"] = info.get("aliases", [])
+                info["aliases"].append(alias)
+
+        return products
+
+    @protect(default=[])
+    def get_events(self, type, *args, **kwargs):
+        """Request to the "events" endpoint.
+
+        The events endpoint allows you to retrieve previous webhook
+        events and process them again (or in some cases for the first)
+        time.
+
+            :param type: "processed" or "unprocessed"
+            :param *args: Passed to each ``get()`` request
+            :param **kwargs: Passed to each ``get()`` request
+        """
+        res = self.get(f"events/{type}", *args, **kwargs)
+        res.raise_for_status()
+        data = res.json()
+
+        for event in data.get("events", []):
+            yield event
+
+        # Drop any 'days' from params - it breaks the next request
         kwargs["params"] = kwargs.get("params", {})
         kwargs["params"].pop("days", None)
 
@@ -70,87 +156,56 @@ class FastSpring(Session):
             timestamp = events[-1].get("created")
             if not(timestamp):
                 break
-            
+
             kwargs["params"].update(begin=timestamp+1)
-            res = self.get(f"events/{type}", **kwargs)
+            res = self.get(f"events/{type}", *args, **kwargs)
 
-            try:
-                res.raise_for_status()
-                data = res.json()
-            except (HTTPError) as error:
-                self.logger.error(
-                    f"Could not get orders from timestamp {timestamp}: {error}"
-                )
-                yield []
-            except (JSONDecodeError) as error:
-                self.logger.error(
-                    f"Could not decode JSON from response ({timestamp}) "
-                    f"{error}"
-                )
-                yield []
-            yield data.get("events")
-
-    def get_orders(self, *args, **kwargs):
-        res = self.get("orders", *args, **kwargs)
-
-        try:
             res.raise_for_status()
             data = res.json()
-        except (HTTPError) as error:
-            self.logger.error(f"Could not get orders: {error}")
-            return []
-        except (JSONDecodeError) as error:
-            self.logger.error(f"Could not decode response JSON: {error}")
-            return []
-
-        yield data.get("orders", [])
             
+            for event in data.get("events", []):
+                yield event
+
+    @protect(default=[])
+    def get_orders(self, *args, **kwargs):
+        """Request to the "orders" endpoint.
+
+        The "orders" endpoint gives you information about orders
+        including those with returns. This is useful for bulk imports.
+
+            :param *args: Passed to each ``get()`` request
+            :param **kwargs: Passed to each ``get()`` request
+        """
+        res = self.get("orders", *args, **kwargs)
+        res.raise_for_status()
+        data = res.json()
+
+        for order in data.get("orders", []):
+            yield order
+
         kwargs["params"] = kwargs.get("params", {})
 
         while data.get("nextPage"):
             page = data.get("nextPage")
             kwargs["params"].update(page=page)
             res = self.get("orders", **kwargs)
-            try:
-                res.raise_for_status()
-                data = res.json()
-            except (HTTPError) as error:
-                self.logger.error(
-                    f"Could not get orders on page {page}: {error}"
-                )
-                yield []
-            except (JSONDecodeError) as error:
-                self.logger.error(
-                    f"Could not decode response JSON on page {page}: "
-                    f"{error}"
-                )
-                yield []
-            yield data.get("orders")
 
-    def get_parents(self, *args, blacklist=["bundle"], **kwargs):
-        products = [name for name in self.get_products(*args, **kwargs)]
-
-        if len(products) == 0:
-            return {}
-
-        parent_information = defaultdict(list)
-        for info in self.get_products(*products):
-            offers = info.get("offers", [])
-
-            if any([offer.get("type") in blacklist for offer in offers]):
-                continue
-
-            parent = info.get("parent")
-            child = info.get("product")
-            if parent and child:
-                parent_information[parent].append(child)
-
-        return parent_information
-
-    def update_event(self, id, **kwargs):
-        res = self.post(f"events/{id}", **kwargs)
-        try:
             res.raise_for_status()
-        except (HTTPError) as error:
-            self.logger.error(f"Could not update event: {error}")
-        return res
+            data = res.json()
+            
+            for order in data.get("orders", []):
+                yield order
+
+    @protect(default={})
+    def update_event(self, id, *args, **kwargs):
+        """Requests POST to the "events" endpoint
+
+        Typically you want to use this to mark an event as "processed".
+
+            :param id: The event's id
+            :param *args: Passed to ``post()`` request
+            :param **kwargs: Passed to ``post()`` request
+        """
+        res = self.post(f"events/{id}", *args, **kwargs)
+        res.raise_for_status()
+        return res.json()
