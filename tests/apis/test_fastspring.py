56475c04d303606459804156f052e27cb7d256cb
Nikolaus Gullotta <nik@harrisonconsoles.com>
2020-03-03 12:50:56 -0600

    Replace requests-mock with pytest-env
    We unfortunately cannot use requests-mock yet. It would be too much time to set up.
    
    Instead we are going to use pytest-env to forward credentials for tests

--- tests/apis/test_fastspring.py
+++ tests/apis/test_fastspring.py
@@ -1,12 +1,108 @@
+from base64 import b64encode, urlsafe_b64encode
+from os import urandom
+from urllib.parse import urlencode, urljoin
+
 import pytest
+from testing.postgresql import Postgresql
 
 from nest.apis.fastspring import FastSpring
+from nest.apis.fastspring.events import EventParser, Order
+from nest.engines.psql import PostgreSQLEngine
+
+
+def random_str(length=16, safe=True):
+    rv, bits = b"", urandom(length)
+    if safe:
+        rv = urlsafe_b64encode(bits)
+    else:
+        rv = b64encode(bits)
+    return rv.decode("utf-8")
+
+@pytest.fixture(scope="module")
+def engine():
+    with Postgresql() as psql:
+        yield PostgreSQLEngine(url=psql.url())
+
+@pytest.fixture()
+def database(engine):
+    session = engine.session()
+    yield session
+    session.close()
 
 @pytest.fixture(scope="module")
 def session():
     yield FastSpring()
 
-def test_fastspring(requests_mock, session):
-    requests_mock.get(f"{session.prefix}/orders", json={"test":1})
-    res = session.get("orders")
-    json = res.json()
+def test_session_hooks(session):
+    counter = 0
+    def callback(*args, **kwargs):
+        nonlocal counter
+        counter += 1
+
+    session.hooks["response"] = callback
+    session.request("GET", "")
+
+    session.hooks.pop("response", None)
+    session.request("GET", "")
+
+    assert(counter == 1)
+
+def test_fastspring_get_products(session):
+    products = []
+    for product, info in session.get_products(blacklist=["bundle"]).items():
+        products.append(product)
+        for key in ["price", "aliases"]:
+            assert(key in info.keys())
+        assert(product in info.get("aliases"))
+
+    if len(products) > 1:
+        p1 = products[0]
+        p2 = products[1]
+        r1 = random_str()
+        r2 = random_str()
+        product_info = session.get_products(
+            filter=[p1, p2, random_str()], 
+            blacklist=["bundle"]
+        )
+
+        for p in [p1, p2]:
+            assert(p in product_info)
+        
+        for p in [r1, r2]:
+            assert(p not in product_info)
+
+    product_info = session.get_products(filter=[random_str()])
+    assert(product_info == {})
+
+def test_fastspring_get_orders(session):
+    params = {
+        "scope": "live",
+        "status": "completed",
+        "begin": "2020-02-27",
+        "end": "2020-02-28",
+        "limit": 50
+    }
+    for order in session.get_orders(params=params):
+        break
+
+def test_fastspring_get_events(session):
+    for event in session.get_events("processed", params={"days":2}):
+        break
+
+def test_fastspring_update_event(session):
+    for event in session.get_events("processed", params={"days":1}):
+        res = session.update_event(
+            event.get("id"), 
+            params={
+                "processed": True
+            }
+        )
+        assert(res.get("processed"))
+        break
+
+def test_order_event(session, database):
+    for event in session.get_events("processed", params={"days":2}):
+        order = Order(event, session=database)
+        database.add(order.model)
+        database.commit()
+        break
--- tests/apis/test_mailchimp.py
+++ tests/apis/test_mailchimp.py
@@ -2,6 +2,30 @@ import pytest
 
 from nest.apis.mailchimp import Mailchimp
 
-def test_mailchimp():
-    session = Mailchimp()
-    assert(session is not None)
+
+@pytest.fixture()
+def session(scope="module"):
+    yield Mailchimp()
+
+def test_mailchimp(session):
+    session.default_list = "Harrison Opted-In Promo List"
+    res = session.get("members")
+    assert(res.ok)
+
+def test_mailchimp_get_members(session):
+    session.default_list = "Harrison Opted-In Promo List"
+    counter = 0
+    for member in session.get_members():
+        counter += 1
+        if counter == 10:
+            break
+
+def test_mailchimp_get_member(session):
+    session.default_list = "Harrison Opted-In Promo List"
+
+    email = ""
+    for member in session.get_members():
+        email = member.get("email_address", "foo@bar.com")
+        break
+    
+    res = session.get_member(email)
